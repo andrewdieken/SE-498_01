d8e49f5c58ed5c7580f1902abdf92e5421be86f7
andrewdieken <dieke102@mail.chapman.edu>
2019-02-18 11:58:34 -0800

    Update API to show results on pages

--- rails-app/app/controllers/api/v1/voters_controller.rb
+++ rails-app/app/controllers/api/v1/voters_controller.rb
@@ -2,18 +2,20 @@ module Api
   module V1
     class VotersController < ApplicationController
       def index
-        users = Voter.order('created_at DESC');
-        render json: {status:'SUCCESS', message:'Loaded Voters', data:users},status: :ok
+        voters = Voter.page(params[:page]).per(2)
+        total_pages = voters.total_pages
+        previous_page = voters.prev_page
+        next_page = voters.next_page
+        render json: {meta: {total_pages: total_pages, previous_page: previous_page, next_page: next_page},status:'SUCCESS', message:'Loaded Voters', data:voters},status: :ok
       end
 
       def show
-        user = Voter.find_by_id(params[:id]);
-        if not user.nil?
-          render json: {status:'SUCCESS', message:'Loaded Voters', data:user},status: :ok
+        voter = Voter.find_by_id(params[:id])
+        if not voter.nil?
+          render json: {status:'SUCCESS', message:'Loaded Voters', data:voter},status: :ok
         else
           render json: {status:'FAILED', message:'Voter Does Not Exist'},status: :unprocessable_entity
         end
-
       end
 
     end
