c33d5700d6bc7fb6290669e8b48f8cff240136fc
Matt McCortney <hazmatt101@gmail.com>
2019-03-06 18:42:02 -0800

    Add migration file that adds additional columns
    to the Voter table and loads in 2000 real voter records

--- rails-app/db/seeds.rb
+++ rails-app/db/seeds.rb
@@ -5,13 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-voterslist = [
-	[1080170, "Aaby", "Dorothy", "V", "2010 Pioneer Ave", "Fullterton", "CA", "92831", "000-680-6234", "", "07/23/1935", "Republican"],
-	[2185680, "Aali", "Seyed", "Mohammad", "28 Deerwood E", "Irvine", "CA", "92604", "949-552-8664", "", "10/03/1930", "Democrat"],
-	[3365293, "Byrne", "Vicki", "", "67 Wisteria Pl", "Aliso Viejo", "CA", "92656", "", "", "01/03/1973", "Republican"],
-	[4154773, "Shieh", "Jiann", "Sheng", "9581 Woodlawn Dr", "Huntington Beach", "CA", "92646", "714-965-8828", "shieh.jiann@gmail.com", "02/11/1951", "N/A"]
-]
+require 'csv'
 
-voterslist.each do |voterid, lastname, firstname, middlename, address, city, state, zip, phone, email, dateofbirth, partyaffiliation |
-	Voter.create(voterID: voterid, last_name: lastname, first_name: firstname, middle_name: middlename, address: address, city: city, state: state, zip: zip, phone: phone, email: email, date_of_birth: dateofbirth, party_affiliation: partyaffiliation)
+fields = %w{lVoterUniqueID sAffNumber szStateVoterID sVoterTitle szNameLast szNameFirst szNameMiddle sNameSuffix sGender szSitusAddress szSitusCity sSitusState sSitusZip sHouseNum sUnitAbbr sUnitNum szStreetName sStreetSuffix sPreDir sPostDir szMailAddress1 szMailAddress2 szMailAddress3 szMailAddress4 szMailZip szPhone szEmailAddress dtBirthDate sBirthPlace dtRegDate dtOrigRegDate dtLastUpdate_dt sStatusCode szStatusReasonDesc sUserCode1 sUserCode2 iDuplicateIDFlag szLanguageName szPartyName szAVStatusAbbr szAVStatusDesc szPrecinctName sPrecinctID sPrecinctPortion sDistrictID_0 iSubDistrict_0 szDistrictName_0 sDistrictID_1 iSubDistrict_1 szDistrictName_1 sDistrictID_2 iSubDistrict_2 szDistrictName_2 sDistrictID_3 iSubDistrict_3 szDistrictName_3 sDistrictID_4 iSubDistrict_4 szDistrictName_4 sDistrictID_5 iSubDistrict_5 szDistrictName_5}
+
+CSV.foreach(Dir.pwd + "/db/Cntywd_020819.csv", headers: true) do |row|
+  voter_row = row.to_hash.select { |k, v| fields.include?(k)}
+  Voter.create!(voter_row.to_hash.symbolize_keys)
 end
