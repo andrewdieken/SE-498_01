33187dd6aff103c8952ba17530705306123add09
andrewdieken <dieke102@mail.chapman.edu>
2019-03-23 00:40:36 -0700

    Add Graphql connection to react-app/src/index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,3 +13,16 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const client = new ApolloClient ({
+  uri: "http://localhost:3000/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+client.query({query: gql `{allVoters {id}}`}).then(result => console.log(result))
