0deb58ce19011d731440fe3d214f10ca7a924161
Matt McCortney <hazmatt101@gmail.com>
2019-03-25 19:42:14 -0700

    Remove old columns from Voter table
    - Remove old and redundant columns from Voter table

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_021619) do
+ActiveRecord::Schema.define(version: 2019_03_26_023343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,18 +28,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_021619) do
   end
 
   create_table "voters", force: :cascade do |t|
-    t.integer "voterID"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "middle_name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
-    t.string "email"
-    t.string "phone"
-    t.string "date_of_birth"
-    t.string "party_affiliation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "lVoterUniqueID"
