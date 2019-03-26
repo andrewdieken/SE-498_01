1419d86aa5fdfc9995ef79f3c482a0c19119002b
Xavi Ablaza <xlablaza@gmail.com>
2019-03-25 21:51:07 -0700

    Voter has_many Visits

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_043508) do
+ActiveRecord::Schema.define(version: 2019_03_26_043922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_03_26_043508) do
   create_table "visits", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "voter_id"
+    t.index ["voter_id"], name: "index_visits_on_voter_id"
   end
 
   create_table "voters", force: :cascade do |t|
@@ -99,4 +101,5 @@ ActiveRecord::Schema.define(version: 2019_03_26_043508) do
     t.string "szDistrictName_5"
   end
 
+  add_foreign_key "visits", "voters"
 end
