27f7321b6f8ee3840999846ddc6392357e425c71
andrewdieken <dieke102@mail.chapman.edu>
2019-03-29 12:08:22 -0700

    Fix rebase issues

--- rails-app/app/graphql/types/mutation_type.rb
+++ rails-app/app/graphql/types/mutation_type.rb
@@ -1,14 +1,5 @@
 module Types
-<<<<<<< HEAD
-<<<<<<< HEAD
-  class Types::MutationType < Types::BaseObject
-=======
   class MutationType < Types::BaseObject
->>>>>>> Enable react app to make calls to Graphql server
     field :create_voter, mutation: Mutations::CreateVoter
-=======
-  class Types::MutationType < Types::BaseObject
-    field :create_voter, mutation: Mutations::CreateVoter, camelize: true
->>>>>>> Add all voter arguments to create voter mustation
   end
 end
