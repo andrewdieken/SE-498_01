5b71ee60687e786884c486703469fc71fa054fe2
andrewdieken <dieke102@mail.chapman.edu>
2019-04-01 18:51:12 -0700

    Add back graphiql development check only

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
   post "/graphql", to: "graphql#execute"
 
   devise_for :users
