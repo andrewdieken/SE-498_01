a04ff97b71439661b83ec366c2b60dc7a65d73b5
andrewdieken <dieke102@mail.chapman.edu>
2019-04-13 13:09:57 -0700

    Update settings spec

--- rails-app/spec/features/settings_spec.rb
+++ rails-app/spec/features/settings_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe "Settings page", type: :feature do
 
-  it "updates the precinct id global variable" do
+  it "updates the precinct id and canvasser_password global variable" do
     visit settings_path
     fill_in "precinct_id", with: 12345
-    click_on "Update"
+    fill_in "canvasser_password", with: "password"
+    click_on "update"
     expect(page).to have_selector("input[value='12345']")
+    expect(page).to have_selector("input[value='password']")
   end
 
 end
