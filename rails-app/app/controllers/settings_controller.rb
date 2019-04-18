06cbaa11559e0e1962c2ddc87ced564a872f0a59
Xavi Ablaza <xlablaza@gmail.com>
2019-04-17 20:06:24 -0700

    Add textbox to admin dashboard
    that updates the default note

--- rails-app/app/controllers/settings_controller.rb
+++ rails-app/app/controllers/settings_controller.rb
@@ -2,11 +2,13 @@ class SettingsController < ApplicationController
   def index
     @precinct_id = Setting['precinct_id']
     @canvasser_password = Setting['canvasser_password']
+    @note = Setting['note']
   end
 
   def update
     Setting.precinct_id = params[:precinct_id]
     Setting.canvasser_password = params[:canvasser_password]
+    Setting.note = params[:note]
     redirect_to settings_path
   end
 
