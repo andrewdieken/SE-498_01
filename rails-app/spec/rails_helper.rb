b1269923111baf61d85b8638927b5498c1e62eb7
Christopher Ruiz <ruiz165@mail.chapman.edu>
2019-04-24 20:24:39 -0700

    devise test controller helper

--- rails-app/spec/rails_helper.rb
+++ rails-app/spec/rails_helper.rb
@@ -15,6 +15,7 @@ module Features
 end
 
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Features, type: :feature
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
