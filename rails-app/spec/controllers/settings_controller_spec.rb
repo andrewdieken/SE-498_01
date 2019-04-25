4506c81e7fd1b21f9f60b225b8f6baabc9fa0bc8
Christopher Ruiz <ruiz165@mail.chapman.edu>
2019-04-24 19:33:06 -0700

    CircleCI test fix

--- rails-app/spec/controllers/settings_controller_spec.rb
+++ rails-app/spec/controllers/settings_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SettingsController, type: :controller do
-
   describe '#index' do
     it 'should get the precinctid value as an array' do
       get :index
       expect(Setting['precinct_id']).to be_an_instance_of(Array)
     end
   end
-
 end
