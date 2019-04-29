932e197b050390e8ab358e16d81b8b89ed54c1bd
lavin105 <lavin105@mail.chapman.edu>
2019-04-29 11:50:17 -0700

    added localstrage for loggin in and remaining logged in, added alert to make sure notes must be added before visiting a voter, added progress bar and localstorage to store the score, added a logout button to remove storage and log out user

--- react-app/src/Classes/authenticate.js
+++ react-app/src/Classes/authenticate.js
@@ -1,12 +1,14 @@
 class Authenticate{
     constructor(){
-        this.authenticated =false;
+        this.authenticated =JSON.parse(localStorage.getItem("loggedIn"))||false;
+        
     }
 
     login(globalPass,userInput,userName,cb){
         if(userName!==""){
             if(globalPass===userInput){
                 this.authenticated =true;
+                localStorage.setItem("loggedIn","true");
                 cb();
             }else{
                 alert("Incorrect password! Please try a new password or contact your campaign manager for the password.");
@@ -18,8 +20,11 @@ class Authenticate{
      
 
     }
-    logout(){
+    logout(cb){
         this.authenticated=false;
+        localStorage.removeItem("loggedIn");
+        localStorage.removeItem("score");
+        cb();
     }
     isAuthenticated(){
         return this.authenticated;
