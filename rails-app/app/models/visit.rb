a406c0b2777b3e9db41200e9e0a35b84eb50a0f4
Xavi Ablaza <xlablaza@gmail.com>
2019-04-28 22:44:47 -0700

    Export voter CSV file with voter data

--- rails-app/app/models/visit.rb
+++ rails-app/app/models/visit.rb
@@ -1,12 +1,22 @@
 class Visit < ApplicationRecord
   belongs_to :voter
-  # Generate a CSV File of the Visits records
+
   def self.to_csv(fields = column_names, options={})
     CSV.generate(options) do |csv|
-    csv << fields
-    all.each do |visits|
-      csv << visits.attributes.values_at(*fields)
+      csv << fields
+      all.each do |visit|
+        csv << [
+          visit.created_at,
+          visit.updated_at,
+          visit.voter.szNameFirst,
+          visit.voter.szNameLast,
+          visit.voter.szSitusAddress,
+          visit.voter.szPhone,
+          visit.voter.szEmailAddress,
+          visit.voter.note
+        ]
+      end
     end
-   end
   end
+
 end
