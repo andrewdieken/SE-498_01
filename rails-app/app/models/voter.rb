34a324321c058b2885418d111fa006cbfc6dc59c
Xavi Ablaza <xlablaza@gmail.com>
2019-05-01 19:54:56 -0700

    Set the voter score

--- rails-app/app/models/voter.rb
+++ rails-app/app/models/voter.rb
@@ -1,3 +1,14 @@
 class Voter < ApplicationRecord
+  before_save :set_random_voter_score
   has_many :visits
+
+  def set_random_voter_score
+    if self.history_score == 0
+      self.history_score = rand(1..10)
+    end
+  end
+
+  def self.save_all
+    Voter.all.each { |voter| voter.save! }
+  end
 end
