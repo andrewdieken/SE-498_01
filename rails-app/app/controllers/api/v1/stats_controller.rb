27c71a8f069c785fca17a5c8d37e8751628e339d
andrewdieken <dieke102@mail.chapman.edu>
2019-05-06 06:59:12 -0700

    Add json respone on success and failure

--- rails-app/app/controllers/api/v1/stats_controller.rb
+++ rails-app/app/controllers/api/v1/stats_controller.rb
@@ -1,14 +1,22 @@
-module API
+module Api
   module V1
     class StatsController < ApplicationController
       def update_houses
-        @new_houses = params[:houses_canvassed]
-        Setting.houses_canvassed = @new_houses
+        @houses = params[:houses].to_i
+        if Setting.houses_canvassed = Setting['houses_canvassed'] + @houses
+          render json: {status: 'SUCCESS'}, status: :ok
+        else
+          render json: {status: 'ERROR'}, status: :unprocessable_entity
+        end
       end
 
       def update_volunteers
-        @new_volunteers
-        Setting.volunteers = Setting['volunteers'] + @new_volunteers
+        @volunteers = params[:volunteers].to_i
+        if Setting.volunteers = Setting['volunteers'] + @volunteers
+          render json: {status: 'SUCCESS'}, status: :ok
+        else
+          render json: {status: 'ERROR'}, status: :unprocessable_entity
+        end
       end
     end
   end
